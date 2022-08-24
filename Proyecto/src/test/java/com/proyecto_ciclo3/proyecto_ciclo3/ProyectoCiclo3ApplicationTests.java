5b16030ee9b30dcf0377ee9c6da924569be3f3ea
Sebastián Ramírez Amador <cbastianramirez@gmail.com>
2022-08-24 17:37:27 -0500

    Se agregan archivos base para proyecto

---
+++ Proyecto/src/test/java/com/proyecto_ciclo3/proyecto_ciclo3/ProyectoCiclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_ciclo3.proyecto_ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCiclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
