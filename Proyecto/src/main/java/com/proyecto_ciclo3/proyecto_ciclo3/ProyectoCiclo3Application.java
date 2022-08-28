6c1a659a8ed4f9f2d7ff90847a52554b64f43f72
Sebastián Ramírez Amador <cbastianramirez@gmail.com>
2022-08-28 14:17:07 -0500

    Se realiza ajustes en visualización de salida de datos para la clase principal/main.

--- Proyecto/src/main/java/com/proyecto_ciclo3/proyecto_ciclo3/ProyectoCiclo3Application.java
+++ Proyecto/src/main/java/com/proyecto_ciclo3/proyecto_ciclo3/ProyectoCiclo3Application.java
@@ -11,7 +11,8 @@ public class ProyectoCiclo3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoCiclo3Application.class, args);
 
-		System.out.println("--------------------------------------------------------------");
+		System.out.println("");
+		System.out.println("----------------------------Enterprise/Empresa----------------------------------");
 
 		Enterprise enterprise1 = new Enterprise(1234567, "Makro", "3005005050", "Calle 4 # 45-34");
 		Enterprise enterprise2 = new Enterprise(8585953, "Flamingo", "55555555", "Carrera 8 # 4-54");
@@ -22,7 +23,7 @@ public class ProyectoCiclo3Application {
 		enterprise1.setId(83748585);
 		System.out.println(enterprise1);
 
-		System.out.println("-------------------------------------------------------------");
+		System.out.println("--------------------------Employee/Empleado------------------------------------");
 
 		Employee employee1 = new Employee(123456, "Pepe Perez", "email@email.com", Enum_RoleName.ADMIN, enterprise1);
 		Employee employee2 = new Employee(785845, "Margarot Ramirez", "email@microsoft.com", Enum_RoleName.OPERARIO, enterprise1);
@@ -33,7 +34,7 @@ public class ProyectoCiclo3Application {
 		employee1.setRole(Enum_RoleName.OPERARIO);
 		System.out.println(employee1);
 
-		System.out.println("-------------------------------------------------------------");
+		System.out.println("-------------------Transaction/MovimientoDinero---------------------------------");
 
 		Transaction transaction1 = new Transaction("Ventas Abril", 150000, employee1);
 		System.out.println(transaction1);
